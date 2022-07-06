7a63063e08c589b4bfbd4203a3fa2d390aa724fc
Grace <grace.anderson.au@gmail.com>
2022-07-06 19:00:05 +1000

    clickHandler used with useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  //function clickHandler
+  //register state using useState()
+  //useState always returns two values
+  //[the value itself, AND the updating function]
+  //call updating function (e.g. setTitle) whenever the state should change
+  //use the value (e.g. title) whenever you want to use the value as an output in JSX code (e.g. <h2>{title}</h2>)
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    setTitle("updated!!!");
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
